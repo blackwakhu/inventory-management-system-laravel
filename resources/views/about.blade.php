2268aad21aeaa7b45a1d4adf2231ab0086621402
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-05-16 07:42:06 +0000

    added some more pages

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,15 @@
 <html>
   <body>
+    <div>
+      <table>
+        <tr>
+          <td><a href="/">Home</a></td>
+          <td><a href="/products">Products</a></td>
+          <td><a href="/customers">Customers</a></td>
+          <td><a href="/about">About</a></td>
+        </tr>
+      </table>
+    </div>
     <h1>The about page</h1>
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/about', function () {
     return view('about');
+});
+
+Route::get('/products', function(){
+    return view('products');
+});
+
+Route::get('/customers', function(){
+    return view('customers');
 });
